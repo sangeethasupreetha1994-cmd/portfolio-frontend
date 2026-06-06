e8602c318bb19f528f9e4e335509d0c9a36d2a79
sangeethasupreetha1994-cmd <sangeethasupreetha1994@gmail.com>
2026-06-06 19:56:58 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Stats from "../components/Stats";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Technologies from "../components/Technologies";
